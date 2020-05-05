1f6822fc1d1baf61c559a6677ef3557ee842dfed
Navdeep Singh <navdeep.singh@hashedin.com>
2020-05-06 02:01:55 +0530

    about to deploy on heroku

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
-
-app = Flask(__name__)
+import sys
+# from flask_cors import CORS
+app = Flask(__name__, static_folder="../../build", static_url_path='/')
 app.debug = True
-
+# CORS(app,support_credentials=True)
+print(sys.path)
 from app import routes
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,17 @@
 from app import app
 from covid import Covid
 from covid_india import states
+# from flask_cors import cross_origin
+
+
+@app.route('/')
+# @cross_origin()
+def index():
+    return app.send_static_file('index.html')
 
 
 @app.route('/country/<country>')
+# @cross_origin()
 def getdata(country):
     covid = Covid()
     # print(country)
@@ -11,6 +19,8 @@ def getdata(country):
     # print(country)
     return {'country':covid.get_status_by_country_name(country)}
 
+
 @app.route('/state/<state>')
+# @cross_origin()
 def get_state(state):
     return {'state':states.getdata(state)}
