20a46e192b29fe84927624b246c2affefdb099e4
Navdeep Singh <navdeep.singh@hashedin.com>
2020-05-06 13:28:10 +0530

    adding build

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
-import sys
 # from flask_cors import CORS
 app = Flask(__name__, static_folder="../../build", static_url_path='/')
 app.debug = True
 # CORS(app,support_credentials=True)
-print(sys.path)
+
 from app import routes
--- backend/covid19.py
+++ backend/covid19.py
@@ -1,2 +1,38 @@
-from app import app
+# from app import app
 
+
+from flask import Flask
+# from flask_cors import CORS
+app = Flask(__name__, static_folder="../../build", static_url_path='/')
+app.debug = True
+# CORS(app,support_credentials=True)
+#
+# from app import routes
+
+
+# from app import app
+from covid import Covid
+from covid_india import states
+# from flask_cors import cross_origin
+
+
+@app.route('/')
+# @cross_origin()
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/country/<country>')
+# @cross_origin()
+def getdata(country):
+    covid = Covid()
+    # print(country)
+    # country=country.replace('-',' ')
+    # print(country)
+    return {'country':covid.get_status_by_country_name(country)}
+
+
+@app.route('/state/<state>')
+# @cross_origin()
+def get_state(state):
+    return {'state':states.getdata(state)}
